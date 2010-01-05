ed9a7480f773aeecc1794aecb2577fabf40e3e14
boris.dadachev <boris.dadachev@04d2028e-e438-11de-8dea-4d08a2fd72f5>
2010-01-05 23:25:04 +0000

    Refonte du menu contextuel du JTree et application de celui ci a la drawZone

---
+++ trunk/src/Listeners/DeleteActionListener.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listeners;
+
+import IHM.DrawPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Boris
+ */
+public class DeleteActionListener implements ActionListener {
+
+    private DrawPanel drawZone;
+
+    public DeleteActionListener(DrawPanel drawZone) {
+        super();
+        this.drawZone = drawZone;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        drawZone.deleteCurrentSelection();
+        drawZone.repaint();
+    }
+}
