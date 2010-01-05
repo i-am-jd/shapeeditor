0ba1dfc4435da18e3fc5cad8fc6c18960f927cc2
boris.dadachev <boris.dadachev@04d2028e-e438-11de-8dea-4d08a2fd72f5>
2010-01-05 22:35:59 +0000

    divers

--- trunk/src/IHM/MenuBar.java
+++ trunk/src/IHM/MenuBar.java
@@ -12,6 +12,7 @@ import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
 public class MenuBar extends JMenuBar {
+
     /* Definition des categories et sous-categories de la barre de menu */
     /* menu Fichier */
 
@@ -22,15 +23,35 @@ public class MenuBar extends JMenuBar {
     private JMenu menuDessin = new JMenu("Dessin");
     private JMenuItem optLancer = new JMenuItem("Lancer");
     /* menu A propos */
-    private JMenu menuAide = new JMenu("A propos");
-    private JMenuItem optAPropos = new JMenuItem("A propos");
+    private JMenu menuAbout = new JMenu("About");
+    private JMenuItem optAbout = new JMenuItem("About");
 
     public MenuBar() {
+        
+        /* Mise en place des raccourcis clavier */
+        optNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
+        optExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, KeyEvent.CTRL_DOWN_MASK));
+
+        /* Ajout des sous-categories aux categories de la barre de menu */
+        this.menuFile.add(optNew);
+        this.menuFile.add(optExit);
+        this.menuDessin.add(optLancer);
+        this.menuAbout.add(optAbout);
+
+        /* Ajout des différents elements de la barre de menu */
+        this.add(menuFile);
+        this.add(menuDessin);
+        this.add(menuAbout);
+    }
+
+    public void initializeListeners(final DrawPanel drawZone)
+    {
         /* Ajout des ActionListener associes aux sous categories */
         optNew.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 Window.sceneGraph.removeAllChildren();
+                drawZone.repaint();
             }
         });
         optExit.addActionListener(new ActionListener() {
@@ -45,34 +66,19 @@ public class MenuBar extends JMenuBar {
                 //...
             }
         });
-        optAPropos.addActionListener(new ActionListener() {
+        optAbout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 JOptionPane jop = new JOptionPane();
                 //ImageIcon img = new ImageIcon("images/img.png");
-                String mess = "Editeur de formes geometriques \n" +
-                        "Projet ILO 2009-2010 \n" +
-                        "Equipe de developpement :\n" +
+                String mess = "Shape Editor \n" +
+                        "ILO Project 2009-2010 \n" +
+                        "Developpement team :\n" +
                         "    - Boris Dadachev\n" +
                         "    - Jean-Denis Koeck\n" +
-                        "Tous droits reserves \n";
-                jop.showMessageDialog(null, mess, "A propos", JOptionPane.INFORMATION_MESSAGE);
+                        "All rights reserved. \n";
+                jop.showMessageDialog(null, mess, "About", JOptionPane.INFORMATION_MESSAGE);
             }
         });
-
-        /* Mise en place des raccourcis clavier */
-        optNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
-        optExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, KeyEvent.CTRL_DOWN_MASK));
-
-        /* Ajout des sous-categories aux categories de la barre de menu */
-        this.menuFile.add(optNew);
-        this.menuFile.add(optExit);
-        this.menuDessin.add(optLancer);
-        this.menuAide.add(optAPropos);
-
-        /* Ajout des différents elements de la barre de menu */
-        this.add(menuFile);
-        this.add(menuDessin);
-        this.add(menuAide);
     }
 }
