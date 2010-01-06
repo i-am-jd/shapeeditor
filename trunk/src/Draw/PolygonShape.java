98e42e740b8129849da5d19cb5d6eeecc75ad705
jdkoeck <jdkoeck@04d2028e-e438-11de-8dea-4d08a2fd72f5>
2010-01-06 15:47:45 +0000

    Interface PolygonShape

---
+++ trunk/src/Draw/PolygonShape.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Draw;
+
+/**
+ *
+ * @author jdkoeck
+ */
+public interface PolygonShape {
+    
+    public int getPointsNb();
+
+    public int getX(int index);
+
+    public int getY(int index);
+
+}
